70545bdfb107f1a8b3156fa8d34dec025a455c45
Willow Weber <weberwillow2010@gmail.com>
2026-05-27 16:49:19 -0700

    Delete build directory

--- build/generated/EmbeddedFonts.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern const unsigned char JetBrainsMonoRegular_ttf[];
-extern const unsigned int JetBrainsMonoRegular_ttf_len;
-
-extern const unsigned char FontAwesomeSolid900_otf[];
-extern const unsigned int FontAwesomeSolid900_otf_len;
